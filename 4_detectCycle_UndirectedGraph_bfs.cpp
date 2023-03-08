6f24c0cd79f81c4d4bc7e2db1e5310e2ee81d0f6
Darshan <darshanheda08@gmail.com>
2023-03-08 06:27:28 +0530

    Detect Cycle Using Bfs in Undirected Graph

---
+++ 4_detectCycle_UndirectedGraph_bfs.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    bool detect(int src, vector<int> adj[], vector<int> vis)
+    {
+        vis[src] = 1;
+        queue<pair<int, int>> q;
+        q.push({src, -1});
+        while (!q.empty())
+        {
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+
+            for (auto it : adj[node])
+            {
+                if (!vis[it])
+                {
+                    vis[it] = 1;
+                    q.push({it, node});
+                }
+                else if (it != parent)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+public:
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[])
+    {
+        // Code here
+        vector<int> vis(V, 0);
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                if (detect(i, adj, vis))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
